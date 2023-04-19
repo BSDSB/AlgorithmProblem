cfa5098f26b9b50bdc37f58de9cc5101d329ef0c
BSDSB <blond617@naver.com>
2023-04-19 10:34:53 +0900

    0419

---
+++ BOJ/BJ7568_덩치.java
@@ -0,0 +1,34 @@
+package BOJ;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BJ7568_덩치 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = sc.nextInt();
+        int[][] input = new int[N][2];
+        int[] prio = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2; j++) {
+                input[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            int cnt = 1;
+            for (int j = 0; j < N; j++) {
+                if(input[i][0] < input[j][0] && input[i][1] < input[j][1])
+                    cnt++;
+            }
+            sb.append(cnt).append(" ");
+        }
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
